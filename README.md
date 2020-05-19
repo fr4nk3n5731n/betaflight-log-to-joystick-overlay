943e3f760b81131883e91cb294531bc7d36cf258
fr4nk3n5731n <erikfreitag8@gmail.com>
2020-05-19 15:30:56 +0200

    added more command line arguments for customisability. Updated README.md to include said arguments

--- README.md
+++ README.md
@@ -5,17 +5,49 @@ I made this because the existing Betaflight Blackbox Explorer is horribly slow i
 
 The green stip in the middle is meant to be keyed out to make it look a little nicer in the video I suppose.
 
-I'll add some more bells as whistles at some point.
 
 ## usage
 
+```
 log-to-mp4.py -i yourinput_file.csv -f 60 -o output.mp4
+```
 
--i|--input PATH: path to input csv file
--f|--framerate FRAMERATE: framerate of the output video. Default: 60fps
--o|--output PATH: path to output video file. Default: output.mp4
+argument defaults will be used if not specified.
+```
+--input|-i INPUT_FILE: path to input CSV file                    
+--out|-o OUTPUT_FILE: path to ouptut video file. Default: ./output.mp4
+--framerate|-r FRAME_RATE: framerate of video. Default: 60
+--height HEIGHT: Height of overlay in pixels. Dfault: 500
+--width WIDTH: Width of overlay in pixels. Default: 1050
+--gap GAP: Size of gap between sticks in pixels. Default: 50
+--gap-colour GAP_COLOUR: colour for the gap between the joysticks for keying out. in video editor. Format "R;G;B" 0-255. Default: 0;255;0
 
-pip dependencies: PIL, numpy, scipy, imageio
+left joystick specific: 
+--left-background-colour LEFT_BACKGROUND_COLOUR: colour for left background format: "R;G;B" 0-255. Default: 0;0;0
+--left-cross-colour LEFT_CROSS_COLOUR: colour for left cross format: "R;G;B" 0-255. Default 255;255;255
+--left-cross-thickness LEFT_CROSS_THICKNESS: thickness for left cross in pixels. Default: 4
+--left-stick-size LEFT_CURSOR_SIZE: size in pixels of the left joystick. Default: 42
+--left-stick-colour LEFT_CURSOR_COLOUR: colour for left joystick format: "R;G;B" 0-255. Default: 255;0;0
+--left_x_range LEFT_X_RANGE: value range of left x axes. Format: "beging:end" Default: -505:505
+--left_y_range LEFT_Y_RANGE: value range of left y axes. Format: "beging:end" Default: 998:2000
+
+right joystick specific:
+--right-background-colour RIGHT_BACKGROUND_COLOUR: colour for right background format: "R;G;B" 0-255. Default: 0;0;0
+--right-cross-colour RIGHT_CROSS_COLOUR: colour for right cross format: "R;G;B" 0-255. Default: 255;255;255
+--right-cross-thickness RIGHT_CROSS_THICKNESS: thickness for right cross in pixels. Default: 4
+--right-stick-size RIGHT_CURSOR_SIZE: size in pixels of the right joystick. Default: 42
+--right-stick-colour RIGHT_CURSOR_COLOUR: colour for right joystick format: "R;G;B" 0-255. Default: 0;0;255
+--right_x_range RIGHT_X_RANGE: value range of right x axes. Format: "beging:end" Default: 505:-505
+--right_y_range RIGHT_Y_RANGE: value range of right y axes. Format: "beging:end" Default: -505:505
+```
+### note:
+The X and Y range is based on the minimum/maximum values I got from my test logs.
+
 
 ### note:
 thanks and have fun
+
+## TODO?:
+* maybe add a UI with preview
+* maybe read data straight from Betaflight Log
+ * decode log data
--- log-to-mp4.py
+++ log-to-mp4.py
@@ -19,19 +19,48 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
-
 import re
 from PIL import Image, ImageDraw
-from typing import List
-import datetime
+from typing import Tuple, List
 from scipy.interpolate import interp1d
 import imageio
 from numpy import asarray
 import argparse
 
+def parse_colour_string(colour_string: str) -> Tuple[int, int, int]:
+	colour = (int(colour_string.split(";")[0]), 
+			  int(colour_string.split(";")[1]), 
+			  int(colour_string.split(";")[2]))
+
+	return colour
+
+def parse_range_string(range_string: str) -> List[int]:
+	input_range = [int(range_string.split(":")[0]),
+				   int(range_string.split(":")[1])]
+
+	return input_range
+
 def decode_to_video(input_file_path: str, 
+					output_file_path: str,
 					frame_rate: int, 
-					output_file_path: str):
+					height: int,
+					width: int,
+					gap: int,
+					gap_colour: Tuple[int, int, int],
+					left_background_colour: Tuple[int, int, int],
+					left_cross_colour: Tuple[int, int, int],
+					left_cross_thickness: int,
+					left_cursor_size: int,
+					left_cursor_colour: Tuple[int, int, int],
+					left_x_range: List[int],
+					left_y_range: List[int],
+					right_background_colour: Tuple[int, int, int],
+					right_cross_colour: Tuple[int, int, int],
+					right_cross_thickness: int,
+					right_cursor_size: int,
+					right_cursor_colour: Tuple[int, int, int],
+					right_x_range: List[int],
+					right_y_range: List[int]):
 	pattern = "^([0-9]+),([0-9]+)(?:[0-9-]+,){12}([0-9-]+),([0-9-]+),([0-9-]+),([0-9-]+).*$"
 	regex = re.compile(pattern)
 	frames = []
@@ -45,36 +74,36 @@ def decode_to_video(input_file_path: str,
 				frames.append(new_frame)
 
 	full_graph = {
-		"height": 500,
-		"width": 1050,
-		"matrix_spacing": 50,
-		"matrix_height": 500,
-		"matrix_width": 500,
-		"background_colour": (0, 255, 0),
+		"height": height,
+		"width": width,
+		"matrix_spacing": gap,
+		"matrix_height": height,
+		"matrix_width": height,
+		"background_colour": gap_colour,
 		"matrixes": [
 			{
-			"background_colour": (0, 0, 0),
-			"line_colour": (255, 255, 255),
-			"line_thiccness": 5,
-			"cursor_colour": (255, 0, 0),
-			"cursor_size": 42,
-			"scale-x": [-501, 501],
-			"scale-y": [998, 2000]
+			"background_colour": left_background_colour,
+			"line_colour": left_cross_colour,
+			"line_thiccness": left_cross_thickness,
+			"cursor_colour": left_cursor_colour,
+			"cursor_size": left_cursor_size,
+			"scale-x": left_x_range,
+			"scale-y": left_y_range
 			},
 			{
-			"background_colour": (0, 0, 0),
-			"line_colour": (255, 255, 255),
-			"line_thiccness": 5,
-			"cursor_colour": (0, 0, 255),
-			"cursor_size": 42,
-			"scale-x": [501, -501],
-			"scale-y": [-501, 501]
+			"background_colour": right_background_colour,
+			"line_colour": right_cross_colour,
+			"line_thiccness": right_cross_thickness,
+			"cursor_colour": right_cursor_colour,
+			"cursor_size": right_cursor_size,
+			"scale-x": right_x_range,
+			"scale-y": right_y_range
 			}
 		]
 	}
-
+	print("=================will video to {0}".format(output_file_path))
 	if frames:
-		frame_rate = 60
+		print("will video to {0}".format(output_file_path))
 		mp4maker = imageio.get_writer(output_file_path, fps=frame_rate)
 		start = frames[0][1]
 		end = frames[-1][1]
@@ -99,7 +128,6 @@ def decode_to_video(input_file_path: str,
 				draw = ImageDraw.Draw(img)
 
 				for matrix in range(0, len(full_graph["matrixes"])):
-					print("========= matrix: {0}".format(matrix))
 					x_min = int(full_graph["matrixes"][matrix]["scale-x"][0])
 					x_max = int(full_graph["matrixes"][matrix]["scale-x"][1])
 					y_min = int(full_graph["matrixes"][matrix]["scale-y"][0])
@@ -170,7 +198,6 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='convert betaflight Blackbox log CSV to Video. And a lot faster then the Blackbox explorer at that.')
 	parser.add_argument('--input', 
 						'-i',
-						required=True,
 						type=str,
 						dest="input_file",
 	                    help='path to input CSV file')
@@ -186,13 +213,127 @@ if __name__ == "__main__":
 						dest="frame_rate",
 						default=60,
 						help="framerate of video")
+	parser.add_argument("--height",
+						type=int,
+						dest="height",
+						default=500,
+						help="Height of overlay in pixels")
+	parser.add_argument("--width",
+						type=int,
+						dest="width",
+						default=1050,
+						help="Width of overlay in pixels")
+	parser.add_argument("--gap",
+						type=int,
+						dest="gap",
+						default=50,
+						help="Size of gap between sticks in pixels")
+	parser.add_argument("--gap-colour",
+						type=str,
+						dest="gap_colour",
+						default="0;255;0",
+						help="colour for the gap between the joysticks for keying out in video editor. Format \"R;G;B\" 0-255.")
 	parser.add_argument('--info',
 						help="small info text")
+	#stuff for the left joystick
+	parser.add_argument("--left-background-colour",
+						type=str,
+						dest="left_background_colour",
+						default="0;0;0",
+						help="colour for left background format: \"R;G;B\" 0-255.")
+	parser.add_argument("--left-cross-colour",
+						type=str,
+						dest="left_cross_colour",
+						default="255;255;255",
+						help="colour for left cross format: \"R;G;B\" 0-255.")
+	parser.add_argument("--left-cross-thickness",
+						type=int,
+						dest="left_cross_thickness",
+						default=4,
+						help="thickness for left cross in pixels.")
+	parser.add_argument("--left-stick-size",
+						type=int,
+						dest="left_cursor_size",
+						default=42,
+						help="size in pixels of the left joystick.")
+	parser.add_argument("--left-stick-colour",
+						type=str,
+						dest="left_cursor_colour",
+						default="255;0;0",
+						help="colour for left joystick format: \"R;G;B\" 0-255.")
+	parser.add_argument("--left_x_range",
+						dest="left_x_range",
+						type=str,
+						default="-505:505",
+						help="value range of left x axes. Format: \"beging:end\"")
+	parser.add_argument("--left_y_range",
+						dest="left_y_range",
+						type=str,
+						default="998:2000",
+						help="value range of left y axes. Format: \"beging:end\"")
+	#stuff for the right joystick
+	parser.add_argument("--right-background-colour",
+						type=str,
+						dest="right_background_colour",
+						default="0;0;0",
+						help="colour for right background format: \"R;G;B\" 0-255.")
+	parser.add_argument("--right-cross-colour",
+						type=str,
+						dest="right_cross_colour",
+						default="255;255;255",
+						help="colour for right cross format: \"R;G;B\" 0-255.")
+	parser.add_argument("--right-cross-thickness",
+						type=int,
+						dest="right_cross_thickness",
+						default=4,
+						help="thickness for right cross in pixels.")
+	parser.add_argument("--right-stick-size",
+						type=int,
+						dest="right_cursor_size",
+						default=42,
+						help="size in pixels of the right joystick.")
+	parser.add_argument("--right-stick-colour",
+						type=str,
+						dest="right_cursor_colour",
+						default="0;0;255",
+						help="colour for right joystick format: \"R;G;B\" 0-255.")
+	parser.add_argument("--right_x_range",
+						dest="right_x_range",
+						type=str,
+						default="505:-505",
+						help="value range of right x axes. Format: \"beging:end\"")
+	parser.add_argument("--right_y_range",
+						dest="right_y_range",
+						type=str,
+						default="-505:505",
+						help="value range of right y axes. Format: \"beging:end\"")
 
 	args = parser.parse_args()
-	print(args.accumulate(args.integers))
 
 	if args.info:
 		info()
+	elif args.input_file:
+		decode_to_video(args.input_file,
+						args.output_file,
+						args.frame_rate,
+						args.height,
+						args.width,
+						args.gap,
+						parse_colour_string(args.gap_colour),
+						parse_colour_string(args.left_background_colour),
+						parse_colour_string(args.left_cross_colour),
+						args.left_cross_thickness,
+						args.left_cursor_size,
+						parse_colour_string(args.left_cursor_colour),
+						parse_range_string(args.left_x_range),
+						parse_range_string(args.left_y_range),
+						parse_colour_string(args.right_background_colour),
+						parse_colour_string(args.right_cross_colour),
+						args.right_cross_thickness,
+						args.right_cursor_size,
+						parse_colour_string(args.right_cursor_colour),
+						parse_range_string(args.right_x_range),
+						parse_range_string(args.right_y_range))
 	else:
-		decode_to_video(args.input_file, args.output_file, args.frame_rate)
+		print("nothing to do")
+						
