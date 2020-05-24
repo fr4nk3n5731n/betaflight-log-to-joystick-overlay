06495190f2df7c4c45e5f217a4caacd64b109b4a
fr4nk3n5731n <erikfreitag8@gmail.com>
2020-05-24 19:41:44 +0200

    fixed video desync. changed default range values for stick inputs. some code cleanup

--- log-to-mp4.py
+++ log-to-mp4.py
@@ -26,6 +26,44 @@ from scipy.interpolate import interp1d
 import imageio
 from numpy import asarray
 import argparse
+import sys
+
+
+def generate_frame_list(frames: List, frame_rate: int):
+		start = frames[0][1]
+		end = frames[-1][1]
+		runtime = (int(end) - int(start)) / 1000000
+		"""
+		current flaws:
+		- there probably is a faster/more efficient way of doing this
+		- only full seconds will be added to the video
+
+		ToDo:
+		- allow usage of partial seconds
+		- eventually find more efficient way to do that
+		"""
+		pre_cooked_frames = []
+		frame_list_location = 0
+		for i in range(0, int(runtime)):
+			samples_for_second = []
+			while (int(frames[frame_list_location][1]) - int(start)) / 1000000 <= i + 1:
+				samples_for_second.append(frames[frame_list_location])
+				frame_list_location = frame_list_location + 1
+			sample_count = len(samples_for_second)
+			if frame_rate > sample_count:
+				sys.stderr.write("framerate exceeds available samples.")
+				sys.stderr.write("target framerate: {0}fps available samples: {1}".format(frame_rate, sample_count))
+				sys.exit(1)
+			frame_selector = interp1d([0,
+						  			   frame_rate],								
+									   [0,
+									   sample_count])
+			new_second = []
+			for n in range(0, frame_rate):
+				new_second.append(samples_for_second[int(frame_selector(n))])
+			pre_cooked_frames.append(new_second)
+			
+		return pre_cooked_frames
 
 def parse_colour_string(colour_string: str) -> Tuple[int, int, int]:
 	colour = (int(colour_string.split(";")[0]), 
@@ -61,7 +99,7 @@ def decode_to_video(input_file_path: str,
 					right_cursor_colour: Tuple[int, int, int],
 					right_x_range: List[int],
 					right_y_range: List[int]):
-	pattern = "^([0-9]+),([0-9]+)(?:[0-9-]+,){12}([0-9-]+),([0-9-]+),([0-9-]+),([0-9-]+).*$"
+	pattern = "^([0-9]+),([0-9]+),(?:[0-9-]+,){11}([0-9-]+),([0-9-]+),([0-9-]+),([0-9-]+).*$" #regex was broken
 	regex = re.compile(pattern)
 	frames = []
 	with open(input_file_path, 'r') as logfile:
@@ -101,29 +139,26 @@ def decode_to_video(input_file_path: str,
 			}
 		]
 	}
-	print("=================will video to {0}".format(output_file_path))
 	if frames:
-		print("will video to {0}".format(output_file_path))
 		mp4maker = imageio.get_writer(output_file_path, fps=frame_rate)
 		start = frames[0][1]
 		end = frames[-1][1]
-		print("first: {0}".format(start))
-		print("last: {0}".format(end))
-		print("length: {0}".format(len(frames)))
-		runtime = (int(end) - int(start)) / 100000
-		print("runtime: {0}s".format(runtime))
+		runtime = (int(end) - int(start)) / 1000000
+		print("target framerate: {0}fps".format(frame_rate))
+		print("runtime: {0}s".format(int(runtime)))
+		print("Saving video to: {0}".format(output_file_path))
+
+		total_frames = len(generate_frame_list(frames, frame_rate)) * frame_rate
+		frames_percentage = interp1d([0,
+						  			 total_frames - 1],								
+									 [0,
+									 100])
 		
-		frame_count = len(frames)
-		frames_for_framerate = frame_count/runtime/frame_rate
-		print("frame time with {0}fps: {1}s".format(frame_rate, runtime/frame_rate))
-		print("frame rate: {0}".format(frame_rate))
-		print("possible frames divided by frametime: {0}".format(frame_count/runtime))
-		print("use every n frame to get framerate: {0}".format(frames_for_framerate))
-		print("total frames at framerate of {0}: {1}".format(frame_rate, frame_count/(frame_count/runtime/frame_rate)))
-		counter = int(frames_for_framerate) - 1
-		for frame in frames:
-			if counter + 1 == int(frames_for_framerate):
-				counter = 0
+		for second_count, second in enumerate(generate_frame_list(frames, frame_rate)):
+			for frame_count, frame in enumerate(second):
+				current_frame = frame_count + second_count * 60
+				sys.stdout.write("\rProgress: {0}% rendering frame {1}/{2}".format(str(frames_percentage(current_frame))[:5], current_frame, total_frames))
+				sys.stdout.flush()
 				img = Image.new("RGB", (full_graph["width"], full_graph["height"]), (0, 255, 0))
 				draw = ImageDraw.Draw(img)
 
@@ -144,21 +179,16 @@ def decode_to_video(input_file_path: str,
 											   [0,
 											   full_graph["matrix_height"]])
 					horizontal_offset = matrix * full_graph["matrix_spacing"] + matrix * full_graph["matrix_width"]
-					print("horizontal_offset: {0}".format(horizontal_offset))
 					background_box = [(0 + horizontal_offset,
 									  0),
 									  (full_graph["matrix_width"] + horizontal_offset, 
 									  full_graph["matrix_height"])]
-					print("point 1x raw value: {0}; mapped value: {1}".format(frame[x_pos], int(cursor_offset_x(frame[x_pos]))))
-					print("point 1y raw value: {0}; mapped value: {1}".format(frame[y_pos], int(cursor_offset_y(frame[y_pos]))))
 					point1 = (int(cursor_offset_x(frame[x_pos])) - (full_graph["matrixes"][matrix]["cursor_size"] / 2) + horizontal_offset,
 							  int(cursor_offset_y(frame[y_pos])) - (full_graph["matrixes"][matrix]["cursor_size"] / 2)
 							 )
 					point2 = (point1[0] + (full_graph["matrixes"][matrix]["cursor_size"] / 2),
 							  point1[1] + (full_graph["matrixes"][matrix]["cursor_size"] / 2)
 							 )
-					print("point1: {0}".format(point1))
-					print("point2: {0}".format(point2))
 
 					cursor_location = [(point1[0],
 									   point1[1]),
@@ -183,10 +213,7 @@ def decode_to_video(input_file_path: str,
 					draw.ellipse(cursor_location, fill=full_graph["matrixes"][matrix]["cursor_colour"])
 
 				mp4maker.append_data(asarray(img))
-			else:
-				counter = counter + 1
 		mp4maker.close()
-
 
 def info():
 	print("Please use the Blacbox Explorer to export your flight log as in CSV.")
@@ -216,18 +243,18 @@ if __name__ == "__main__":
 	parser.add_argument("--height",
 						type=int,
 						dest="height",
-						default=500,
-						help="Height of overlay in pixels")
+						default=512,
+						help="Height of overlay in pixels (recommended to a multiple of 8)")
 	parser.add_argument("--width",
 						type=int,
 						dest="width",
-						default=1050,
-						help="Width of overlay in pixels")
+						default=1088,
+						help="Width of overlay in pixels (recommended to a multiple of 8)")
 	parser.add_argument("--gap",
 						type=int,
 						dest="gap",
-						default=50,
-						help="Size of gap between sticks in pixels")
+						default=64,
+						help="Size of gap between sticks in pixels (recommended to a multiple of 8)")
 	parser.add_argument("--gap-colour",
 						type=str,
 						dest="gap_colour",
@@ -264,12 +291,12 @@ if __name__ == "__main__":
 	parser.add_argument("--left_x_range",
 						dest="left_x_range",
 						type=str,
-						default="-505:505",
+						default="-520:520",
 						help="value range of left x axes. Format: \"beging:end\"")
 	parser.add_argument("--left_y_range",
 						dest="left_y_range",
 						type=str,
-						default="998:2000",
+						default="990:2020",
 						help="value range of left y axes. Format: \"beging:end\"")
 	#stuff for the right joystick
 	parser.add_argument("--right-background-colour",
@@ -300,12 +327,12 @@ if __name__ == "__main__":
 	parser.add_argument("--right_x_range",
 						dest="right_x_range",
 						type=str,
-						default="505:-505",
+						default="520:-520",
 						help="value range of right x axes. Format: \"beging:end\"")
 	parser.add_argument("--right_y_range",
 						dest="right_y_range",
 						type=str,
-						default="-505:505",
+						default="-520:520",
 						help="value range of right y axes. Format: \"beging:end\"")
 
 	args = parser.parse_args()
